7f96f60352b2c9638f93469efe2730ddd09b30ad
chenchaofeng <1304642992@qq.com>
2023-05-25 12:36:57 +0800

    Create LoginTicket.java

---
+++ src/main/java/com/nowcoder/community/entity/LoginTicket.java
@@ -0,0 +1,70 @@
+package com.nowcoder.community.entity;
+
+import java.util.Date;
+
+/**
+ * 对应数据库中
+ */
+public class LoginTicket {
+    //对应主键
+    private int id;
+    //用户id
+    private int userId;
+    //登录凭证
+    private String ticket;
+    //用户登录状态
+    private int status;
+    //登录状态过期时间
+    private Date expired;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getTicket() {
+        return ticket;
+    }
+
+    public void setTicket(String ticket) {
+        this.ticket = ticket;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public Date getExpired() {
+        return expired;
+    }
+
+    public void setExpired(Date expired) {
+        this.expired = expired;
+    }
+
+    @Override
+    public String toString() {
+        return "LoginTicket{" +
+                "id=" + id +
+                ", userId=" + userId +
+                ", ticket='" + ticket + '\'' +
+                ", status=" + status +
+                ", expired=" + expired +
+                '}';
+    }
+}
